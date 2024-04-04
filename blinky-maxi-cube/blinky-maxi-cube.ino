c6d8c4c006195a37746eb420aa7d63da069b8cc4
David McGinnis <david.mcginnis@bl-mc.se>
2024-04-04 10:26:40 +0200

    added cycling code

--- blinky-maxi-cube/blinky-maxi-cube.ino
+++ blinky-maxi-cube/blinky-maxi-cube.ino
@@ -1,10 +1,10 @@
-const boolean CHATTY_CATHY  = false;
-const char*   MQTT_SERVER   = "192.168.1.156";
-const char*   MQTT_USERNAME = "blinky-lite-box-01";
-const char*   MQTT_PASSWORD = "areallybadpassword";
-const char*   BOX           = "blinky-lite-box-01";
+const boolean CHATTY_CATHY  = true;
+const char*   MQTT_SERVER   = "192.168.1.1";
+const char*   MQTT_USERNAME = "bl-iter-01";
+const char*   MQTT_PASSWORD = "andLetThereBeLite!";
+const char*   BOX           = "bl-iter-01";
 const char*   TRAY_TYPE     = "blinky-mqtt";
-const char*   TRAY_NAME     = "maxi-01";
+const char*   TRAY_NAME     = "maxi-03";
 const char*   HUB           = "cube";
 
 #include <Controllino.h>  
@@ -14,12 +14,13 @@ union CubeData
   {
     int16_t state;
     int16_t watchdog;
-    int16_t linoD0;
-    int16_t linoD1;
-    int16_t linoR0;
-    int16_t linoR1;
+    int16_t valveCycleState;
+    int16_t valveNumCycles;
+    int16_t valveCycleInterval;  //seconds
+    int16_t valveCycleCount;
+    int16_t valveState;
   };
-  byte buffer[12];
+  byte buffer[14];
 };
 CubeData cubeData;
 byte mac[] = { 0x42, 0x4C, 0x30, 0x30, 0x30, 0x31 };
@@ -27,7 +28,9 @@ byte mac[] = { 0x42, 0x4C, 0x30, 0x30, 0x30, 0x31 };
 #include "BlinkyEtherCube.h"
 
 unsigned long lastPublishTime;
+unsigned long lastValveCycleTime;
 unsigned long publishInterval = 2000;
+unsigned long valveCycleInterval = 0; //mS
 
 void setup()
 {
@@ -42,7 +45,7 @@ void setup()
   BlinkyEtherCube.setMqttRetryMs(3000);
   BlinkyEtherCube.setBlMqttKeepAlive(8);
   BlinkyEtherCube.setBlMqttSocketTimeout(4);
-  
+ 
   // Must be included
   BlinkyEtherCube.setMqttServer(mac, MQTT_SERVER, MQTT_USERNAME, MQTT_PASSWORD);
   BlinkyEtherCube.setMqttTray(BOX,TRAY_TYPE,TRAY_NAME, HUB);
@@ -51,27 +54,24 @@ void setup()
   lastPublishTime = millis();
   cubeData.state = 1;
   cubeData.watchdog = 0;
-  cubeData.linoD0 = 0;
-  cubeData.linoD1 = 0;
-  cubeData.linoR0 = 0;
-  cubeData.linoR1 = 0;
+  cubeData.valveCycleState = 0;
+  cubeData.valveNumCycles = 5;
+  cubeData.valveCycleCount = 0;
+  cubeData.valveCycleInterval = 30;
+  valveCycleInterval = ((unsigned long) cubeData.valveCycleInterval) * 1000;
+  cubeData.valveState = 0;
 
-  pinMode(CONTROLLINO_D0, OUTPUT);
-  pinMode(CONTROLLINO_D1, OUTPUT); 
-  pinMode(CONTROLLINO_R0, OUTPUT); 
-  pinMode(CONTROLLINO_R1, OUTPUT); 
-   
-  digitalWrite(CONTROLLINO_D0, cubeData.linoD0);   
-  digitalWrite(CONTROLLINO_D1, cubeData.linoD1);   
-  digitalWrite(CONTROLLINO_R0, cubeData.linoR0);   
-  digitalWrite(CONTROLLINO_R1, cubeData.linoR1);   
+  pinMode(CONTROLLINO_D0, OUTPUT);    
+  digitalWrite(CONTROLLINO_D0, cubeData.valveState);    
+  lastValveCycleTime = lastPublishTime;
 
 }
 
 void loop()
 {
   unsigned long nowTime = millis();
-  
+  valveCycle42(nowTime);
+ 
   if ((nowTime - lastPublishTime) > publishInterval)
   {
     lastPublishTime = nowTime;
@@ -81,6 +81,26 @@ void loop()
   }  
   BlinkyEtherCube.loop();
 }
+
+void valveCycle42(unsigned long nowTime)
+{
+  if (cubeData.valveCycleState != 42) return;
+  if ((nowTime - lastValveCycleTime) < valveCycleInterval) return;
+  if (cubeData.valveState == 0)
+  {
+    cubeData.valveState = 1;
+  }
+  else
+  {
+    cubeData.valveState = 0;
+  }
+  digitalWrite(CONTROLLINO_D0, cubeData.valveState); 
+  lastValveCycleTime = nowTime;  
+  cubeData.valveCycleCount = cubeData.valveCycleCount + 1;
+// end cycling if we reach max cycle count
+  if (cubeData.valveCycleCount >= cubeData.valveNumCycles) cubeData.valveCycleState = 0;
+}
+
 void handleNewSettingFromServer(uint8_t address)
 {
   switch(address)
@@ -90,17 +110,28 @@ void handleNewSettingFromServer(uint8_t address)
     case 1:
       break;
     case 2:
-      digitalWrite(CONTROLLINO_D0, cubeData.linoD0);  
+      switch(cubeData.valveCycleState)
+      {
+        case 0:
+          break;
+        case 42:
+          cubeData.valveCycleCount = 0;
+          break;
+        default:
+          break;
+      }
       break;
     case 3:
-      digitalWrite(CONTROLLINO_D1, cubeData.linoD1);    
-      break;
+      if (cubeData.valveNumCycles < 0) cubeData.valveNumCycles = 0;
+      break;                
     case 4:
-      digitalWrite(CONTROLLINO_R0, cubeData.linoR0);  
-      break;
-    case 5:
-      digitalWrite(CONTROLLINO_R1, cubeData.linoR1);    
-      break;
+      if (cubeData.valveCycleInterval < 1) cubeData.valveNumCycles = 1;
+      valveCycleInterval = ((unsigned long) cubeData.valveCycleInterval) * 1000;
+      break;                
+    case 6:
+      digitalWrite(CONTROLLINO_D0, cubeData.valveState); 
+      lastValveCycleTime = millis();  
+      break;                
     default:
       break;
   }
